00399b19111f3f3fb8f0111724d6a1269aec0620
PawanFartyal <pawan.fartyal@techsuperiors.com>
2024-05-31 14:40:07 +0530

    contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,14 @@
+import style from '@/styles/contact.module.css';
 export default function Contact(){
     return (
-        <div style={{textAlign:"center",}}>
-            <h1>Contact</h1>
+        <div className={style.contactContainer}>
+            <h1>Contact Us</h1>
+            <form className={style.contactForm}>
+                <input type='text' placeholder='Name' />
+                <input type='email' placeholder='Email' />
+                <textarea rows={20} cols={30} placeholder='Message' />
+                <button className={style.submitBtn} >Submit</button>
+            </form>
             </div>
     )
   }
