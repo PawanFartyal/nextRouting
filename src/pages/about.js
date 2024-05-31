3542d2222406c26a7297b0a183566a62a809dbb7
PawanFartyal <pawan.fartyal@techsuperiors.com>
2024-05-31 12:26:22 +0530

    about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,20 @@
-export default function About(){
-    return (
-      <div style={{textAlign:"center",}}><h1>About</h1></div>
-    )
-  }
-  
+import style from "@/styles/about.module.css";
+export default function About() {
+  return (
+    <div className={style.aboutContainer}>
+      <div className={style.content}>
+        <h1>About Me</h1>
+        <p>
+          I'm a paragraph. Click here to add your own text and edit me. It’s
+          easy. Just click “Edit Text” or double click me to add your own
+          content and make changes to the font. Feel free to drag and drop me
+          anywhere you like on your page. ​ ​</p>
+         <p>This is a great space to write long text about your company and your services. You can use this space
+          to go into a little more detail about your company. Talk about your
+          team and what services you provide.
+        </p>
+      </div>
+      <img src="profile.webp" />
+    </div>
+  );
+}
